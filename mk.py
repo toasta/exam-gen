bd067fdf423e4ba0112755dd1de9b63011361431
Benedikt Fraunhofer <benedikt.fraunhofer@dc1.com>
2022-04-01 22:41:02 +0200

    evo

--- mk.py
+++ mk.py
@@ -19,8 +19,8 @@ q=[
         'q': 'Welche Hashfunktionen gelten als unsicher?',
         'a': [
             { 'ap': 'sha1', 'sol': True },
-            { 'ap': 'md5', 'sol': True },
-            { 'ap': 'blake2b', 'sol': False },
+            #{ 'ap': 'md5', 'sol': True },
+            #{ 'ap': 'blake2b', 'sol': False },
             { 'ap': 'sha2', 'sol': False },
         ]
     },
@@ -58,55 +58,67 @@ def get_iv(b):
     return h
 
 
-qco = 0
-for i in random.sample(q, len(q)):
-    qco += 1
-    q = i['q']
+def doit():
+
+    max_answers = -1
     init=grh(16)
     key=get_key(init)
     iv=get_iv(init)
-    a=init + bytes.fromhex(i['id'])
+    qco = 0
 
-    of=TMPD + grh(16).hex() + ".png"
+    data = {}
+    data['question_order'] = []
+    data['answers'] = {}
+
+    qlatex = []
+    random.seed(43123)
+    scard = []
+    for i in random.sample(q, len(q)):
+        tmp_latex = {}
+        tmp_latex['question'] = i['q']
+        tmp_latex['answers'] = []
+
+        tmp_score = {'question': [], 'answers': [] }
+        tmp_score['question'].append(i['id'])
+
+        nanswers = len(i['a'])
+        if nanswers > max_answers:
+            max_answers  = nanswers
+
+        for j in i['a']:
+            tmp_score['answers'].append(j['sol'])
+            tmp_latex['answers'].append(j['ap'])
+
+        scard.append(tmp_score)
+        qlatex.append(tmp_latex)
 
 
-    o = {}
-    o['q'] = q
-    o['a'] = []
-    o['qco'] = qco
-    answers = random.sample(i['a'], len(i['a']))
-    cor=[]
-    cor=0
-    co=0
-    for j in answers:
-        if j['sol']:
-            cor |= (1<<co)
-        o['a'].append(j['ap'])
-        co += 1
-
-
-    a=bytes.fromhex(i['id']) + struct.pack("!L", cor)
     cmd=["openssl", "enc", "-chacha20", "-iv", iv, "-nosalt", "-K", key]
-    a=subprocess.run(cmd, capture_output=True, input=a).stdout
+    a = json.dumps(scard)
+    crypted=subprocess.run(cmd, capture_output=True, input=a.encode('utf8')).stdout
+    of='/dev/shm/a.png'
 
-    b = init + a
     cmd=['qrencode', 
         '-t', 'png',
         '-o', of,
         '-d', '600',
         '--8bit',
         ]
-        #'-l', 'L',
-    subprocess.run(cmd, input=a).stdout
-    o['qr'] = r'\includegraphics[width=2cm]{' + of + '}'
-    ret.append(o)
+    subprocess.run(cmd, input=crypted).stdout
+    rscore = {}
+    rscore['basics'] = {
+        'qr': r'\includegraphics[width=8cm]{' + of + '}',
+        'max_answers': max_answers,
+        'max_answers4latex': " ".join(['| l ' for i in range(max_answers)]),
+    }
+    rscore['card'] = scard
 
-
-
-tmpl = env.get_template('t.tex')
-print(tmpl.render(ret=ret))
+    tmpl = env.get_template('t.tex')
+    print(tmpl.render(qlatex=qlatex, score=rscore))
 
 
 
 
     
+
+doit()
