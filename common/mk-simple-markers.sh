dd32deeb1f47bbea696e7c05e8b3ae305c8d4dc5
Benedikt Fraunhofer <benedikt.fraunhofer@dc1.com>
2022-04-17 15:09:21 +0200

    more-random

--- common/mk-simple-markers.sh
+++ common/mk-simple-markers.sh
@@ -1,31 +1,41 @@
 
-SIZE=6
+SIZE=7
 
 function foo()
 {
 NN=$1
-shift
-PX=$1
-shift
-PY=$1
 shift
 
 echo -n "convert -size ${SIZE}x$SIZE xc:white -fill black "
 
 SIZEM1=$(( $SIZE - 1 ))
 
-seq 0 5 | while read a; do
-    echo -n " -draw 'point 0,$a'"
-    echo -n " -draw 'point $SIZEM1,$a'"
-    echo -n " -draw 'point $a,0'"
-    echo -n " -draw 'point $a,$SIZEM1'"
+
+#THIS IS WAAAY Overkill, a simple bitstring would do but bash... dunno
+HH=$(echo "FOOBAR $NN" | openssl dgst -blake2b512 -hex -r | cut -d" " -f 1)
+
+
+co=$(( $SIZEM1 * $SIZEM1 - 1))
+while [[ $co -ge 0 ]]; do
+    t=${HH:$co:1}
+    l=$(( $co / $SIZEM1 + 1))
+    c=$(( $co % $SIZEM1 + 0))
+    if [[ "$t" > "7" ]]; then
+        echo -n " -draw 'point $l,$c'"
+        true
+    fi
+    co=$(( $co - 1 ))
 done
 
-for i in 1 2 ; do
-    for j in 1 2; do
-        echo -n " -draw 'point $(( $PX + $i)),$(( $PY + $j))'"
-    done
+co=$SIZEM1
+while [[ $co -ge 0 ]]; do
+    if [[ $(( $co % 2 )) == 0 ]]; then
+        echo -n " -draw 'point 0,$co'"
+        echo -n " -draw 'point $co, $SIZEM1'"
+    fi
+    co=$(( $co - 1 ))
 done
+
 echo -n " out/$NN.png"
 echo
 echo "convert out/$NN.png -border 2 -scale 1000% out/${NN}_1000.png"
@@ -33,7 +43,7 @@ echo "convert out/$NN.png -border 2 -scale 1000% out/${NN}_1000.png"
 
 mkdir -p out/
 
-foo "0" "0" "0"
-foo "1" "2" "0"
-foo "2" "0" "2"
-foo "3" "2" "2"
+foo "0" 
+foo "1" 
+foo "2" 
+foo "3"
