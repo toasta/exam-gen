b2a0728b6f58bd38fce78f6d094dd45162ea1560
Benedikt Fraunhofer <benedikt.fraunhofer@dc1.com>
2022-04-18 13:28:24 +0200

    evo

--- common/mk-simple-markers.sh
+++ common/mk-simple-markers.sh
@@ -1,5 +1,5 @@
 
-SIZE=4
+SIZE=7
 
 function foo()
 {
@@ -8,18 +8,20 @@ shift
 
 echo -n "convert -size ${SIZE}x$SIZE xc:white -fill black "
 
+SIZEM0=$(( $SIZE - 0 ))
 SIZEM1=$(( $SIZE - 1 ))
+SIZEM2=$(( $SIZE - 2 ))
 
 
 #THIS IS WAAAY Overkill, a simple bitstring would do but bash... dunno
 HH=$(echo "FOOBAR $NN" | openssl dgst -blake2b512 -hex -r | cut -d" " -f 1)
 
 
-co=$(( $SIZEM1 * $SIZEM1 - 1))
+co=$(( $SIZEM0 * $SIZEM0))
 while [[ $co -ge 0 ]]; do
     t=${HH:$co:1}
-    l=$(( $co / $SIZEM1 + 1))
-    c=$(( $co % $SIZEM1 + 0))
+    l=$(( $co / $SIZEM0 + 0))
+    c=$(( $co % $SIZEM0 + 0))
     if [[ "$t" > "7" ]]; then
         echo -n " -draw 'point $l,$c'"
         true
@@ -27,14 +29,26 @@ while [[ $co -ge 0 ]]; do
     co=$(( $co - 1 ))
 done
 
-co=$SIZEM1
-while [[ $co -ge 0 ]]; do
-    if [[ $(( $co % 2 )) == 0 ]]; then
-        echo -n " -draw 'point 0,$co'"
-        echo -n " -draw 'point $co, $SIZEM1'"
-    fi
-    co=$(( $co - 1 ))
+echo -n " -draw 'point 0,0'"
+echo -n " -draw 'point $SIZEM1,0'"
+echo -n " -draw 'point 0, $SIZEM1'"
+echo -n " -draw 'point $SIZEM1,$SIZEM1'"
+
+HALF=$(($SIZE / 2))
+for i in -1 0 1; do
+for j in -1 0 1; do
+    x=$(( $HALF + $i ))
+    y=$(( $HALF + $j ))
+    echo -n " -draw 'point $x,$y'"
 done
+done
+
+#co=$SIZEM1
+#while [[ $co -ge 0 ]]; do
+#        echo -n " -draw 'point $co,$co'"
+#        #echo -n " -draw 'point $co, $SIZEM1'"
+#    co=$(( $co - 1 ))
+#done
 
 echo -n " out/$NN.png"
 echo
