1cc1380acd02ce7f70778bdb01d6bf1068e3f9b1
Benedikt Fraunhofer <benedikt.fraunhofer@dc1.com>
2022-05-06 19:23:00 +0200

    own markers for exact size and no x-y jiggle

--- common/mk-aruco-markers-smaller.py
+++ common/mk-aruco-markers-smaller.py
@@ -1,10 +1,12 @@
-import cv2.aruco
 import re
-import cv2
-import numpy as np
 import subprocess
+import sys
+import numpy as np
+import cv2.aruco
+import cv2
 
 
+# 5 markers is the lowest for size 3
 num_markers = 5
 size=3
 border_bits = 1
@@ -33,6 +35,51 @@ np.savez("cd.npz", nmarkers=num_markers,
     bytesList=a.bytesList,
     )
 
+sscale=10000
+
+def empty_and_checked():
+    imsize=(5*2,5*2)
+    sscale=1000
+    tag = np.ones(imsize, dtype="uint8")
+    tag[tag > 0] = 255
+    to=imsize[0]-1
+    for i in range(0, to):
+        tag[i][0] = 0
+        tag[i][to] = 0
+        tag[to][i] = 0
+        tag[0][i] = 0
+    tag[to][to] = 0
+
+    i="pos_empty"
+    of1=f'out/orig_{i}.png'
+    of2=f'out/{sscale}_{i}.png'
+    of3=f'out/{i}.png'
+    cv2.imwrite(of1, tag)
+    subprocess.run(['convert', of1, 
+             '-background', 'white',
+             '-bordercolor', 'white',
+             '-border', '1',
+            of3])
+    subprocess.run(['convert', of1, '-scale', str(sscale) + "%", of2])
+
+    for i in range(1, to):
+        tag[i][i] = 0
+        tag[to-i][i] = 0
+
+    i="pos_checked"
+    of1=f'out/orig_{i}.png'
+    of2=f'out/{sscale}_{i}.png'
+    of3=f'out/{i}.png'
+    cv2.imwrite(of1, tag)
+    subprocess.run(['convert', of1, 
+             '-background', 'white',
+             '-bordercolor', 'white',
+             '-border', '1',
+            of3])
+    subprocess.run(['convert', of1, '-scale', str(sscale) + "%", of2])
+
+
+empty_and_checked()
 
 for i in range(num_markers):
     tag = np.zeros(imsize, dtype="uint8")
@@ -48,3 +95,4 @@ for i in range(num_markers):
          '-border', '1',
         of3])
     subprocess.run(['convert', of1, '-scale', '1000%', of2])
+
