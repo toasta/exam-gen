bcc668c0011f85a3937f0bf85fa7db7717846c70
Benedikt Fraunhofer <benedikt.fraunhofer@dc1.com>
2022-05-06 19:47:58 +0200

    this is fucked

--- lib/Mcolor.py
+++ lib/Mcolor.py
@@ -3,8 +3,8 @@ import numpy as np
 
 class Mcolor:
     def __init__(self,
-        basecolor=[32,32,128],
-        angle_increment=30):
+        basecolor=[3,7,255],
+        angle_increment=360/5):
 
         self.bc = cv2.cvtColor(
             np.uint8([[basecolor]]),
@@ -14,7 +14,7 @@ class Mcolor:
     def get(self, it):
         _ = self.bc
         _[0] += self.angle_increment * it
-        return _.tolist()
+        return cv2.cvtColor([[_]], cv2.COLOR_HSV2RGB)
 
 if __name__ == "__main__":
     m = Mcolor()
